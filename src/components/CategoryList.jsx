69de394b6288d088bbaf0266e9e9de917a5ce907
Rodrigo <contactorodrigocabrera@gmail.com>
2023-08-14 12:03:24 -0300

    Refactorización de CategoryList creando componente CategoryCard para mejor renderizado.

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { imgs , categories } from '../data'
+import CategoryCard from './CategoryCard'
 
 const [ 
     imgKanto, 
@@ -18,108 +19,82 @@ const CategoryList = () => {
         <div className='flex flex-wrap flex-row justify-center gap-4 mt-10'>
 
             {/* Category Link Kanto */}
-            <Link 
+{/*             <Link 
                 to={`/category/${categories.kanto}`} 
                 className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-purple-500 to-pink-500 transition-all hover:scale-105'>
                 <div className='p-5 flex justify-center items-center'>
-                    <img src={imgKanto} alt={`Categoria ${categories.kanto}`} className='w-screen'/>
+                    <img 
+                    src={imgKanto} 
+                    alt={`Categoria ${categories.kanto}`} 
+                    className='w-screen'/>
                 </div>
 
                 <h1 className="text-center text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5">
                     {categories.kanto}
                 </h1>
-            </Link>
+            </Link> */} {/* Todo el link fue construido primero, y luego se lo refactoriza en componente CategoryCard. */}
+            <CategoryCard 
+                category={categories.kanto} /* props */
+                src={imgKanto} 
+                alt={`Categoria ${categories.kanto}`}
+                gradientColor='from-purple-500 to-pink-500'
+                />
 
             {/* Category Link Johto */}
-            <Link 
-                to={`/category/${categories.johto}`} 
-                className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-pink-500 to-red-500 transition-all hover:scale-105'>
-                <div className='p-5 flex justify-center items-center'>
-                    <img src={imgJohto} alt={`Categoria ${categories.johto}`} className='w-screen'/>
-                </div>
-
-                <h1 className="text-center text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5">
-                    {categories.johto}
-                </h1>
-            </Link>
+            <CategoryCard 
+                category={categories.johto} /* props */
+                src={imgJohto} 
+                alt={`Categoria ${categories.johto}`}
+                gradientColor='from-pink-500 to-red-500'
+                />
 
             {/* Category Link Hohen */}
-            <Link 
-                to={`/category/${categories.hohen}`} 
-                className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-red-500 to-blue-500 transition-all hover:scale-105'>
-                <div className='p-5 flex justify-center items-center'>
-                    <img src={imgHohen} alt={`Categoria ${categories.hohen}`} className='w-screen'/>
-                </div>
-
-                <h1 className="text-center text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5">
-                    {categories.hohen}
-                </h1>
-            </Link>
+            <CategoryCard 
+                category={categories.hohen} /* props */
+                src={imgHohen} 
+                alt={`Categoria ${categories.hohen}`}
+                gradientColor='from-red-500 to-blue-500'
+                />
 
             {/* Category Link Sinnoh */}
-            <Link 
-                to={`/category/${categories.sinnoh}`} 
-                className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-blue-500 to-yellow-500 transition-all hover:scale-105'>
-                <div className='p-5 flex justify-center items-center'>
-                    <img src={imgSinnoh} alt={`Categoria ${categories.sinnoh}`} className='w-screen'/>
-                </div>
-
-                <h1 className="text-center text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5">
-                    {categories.sinnoh}
-                </h1>
-            </Link>
+            <CategoryCard 
+                category={categories.sinnoh} /* props */
+                src={imgSinnoh} 
+                alt={`Categoria ${categories.sinnoh}`}
+                gradientColor='from-blue-500 to-yellow-500'
+                />
 
             {/* Category Link Teselia */}
-            <Link 
-                to={`/category/${categories.teselia}`} 
-                className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-yellow-500 to-lime-500 transition-all hover:scale-105'>
-                <div className='p-5 flex justify-center items-center'>
-                    <img src={imgTeselia} alt={`Categoria ${categories.teselia}`} className='w-screen'/> {/* Revisar imagen porque se ve un tanto pequeña. */}
-                </div>
-
-                <h1 className="text-center text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5">
-                    {categories.teselia}
-                </h1>
-            </Link>
+            <CategoryCard 
+                category={categories.teselia} /* props */
+                src={imgTeselia} 
+                alt={`Categoria ${categories.teselia}`}
+                gradientColor='from-yellow-500 to-lime-500'
+                />
 
             {/* Category Link Kalos */}
-            <Link 
-                to={`/category/${categories.kalos}`} 
-                className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-lime-500 to-teal-500 transition-all hover:scale-105'>
-                <div className='p-5 flex justify-center items-center'>
-                    <img src={imgKalos} alt={`Categoria ${categories.kalos}`} className='w-screen'/>
-                </div>
-
-                <h1 className="text-center text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5">
-                    {categories.kalos}
-                </h1>
-            </Link>
+            <CategoryCard 
+                category={categories.kalos} /* props */
+                src={imgKalos} 
+                alt={`Categoria ${categories.kalos}`}
+                gradientColor='from-lime-500 to-pink-500'
+                />
 
             {/* Category Link Alola */}
-            <Link 
-                to={`/category/${categories.alola}`} 
-                className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-teal-500 to-purple-500 transition-all hover:scale-105'>
-                <div className='p-5 flex justify-center items-center'>
-                    <img src={imgAlola} alt={`Categoria ${categories.alola}`} className='w-screen'/>
-                </div>
-
-                <h1 className="text-center text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5">
-                    {categories.alola}
-                </h1>
-            </Link>
+            <CategoryCard 
+                category={categories.alola} /* props */
+                src={imgAlola} 
+                alt={`Categoria ${categories.alola}`}
+                gradientColor='from-pink-500 to-orange-500'
+                />
 
             {/* Category Link Galar */}
-            <Link 
-                to={`/category/${categories.galar}`} 
-                className='flex flex-col justify-between basis-1/4 bg-slate-500 rounded-2xl overflow-hidden bg-gradient-to-r from-teal-500 to-purple-500 transition-all hover:scale-105'>
-            <div className='p-5 flex justify-center items-center'>
-                <img src={imgGalar} alt={`Categoria ${categories.galar}`} className='w-screen'/>
-                </div>
-
-                <h1 className="text-center text-2xl font-semibold text-stone-100 bg-stone-800 bg-opacity-60 p-3 px-5">
-                    {categories.galar}
-                </h1>
-            </Link>
+            <CategoryCard 
+                category={categories.galar} /* props */
+                src={imgGalar} 
+                alt={`Categoria ${categories.galar}`}
+                gradientColor='from-orange-500 to-lime-500'
+                />
         </div>
     )
 }
