7106505ec2bc0b6a3929539af404c96d8edd5593
Rodrigo <contactorodrigocabrera@gmail.com>
2023-08-14 14:07:41 -0300

    Visualización de respuestas correcta e incorrectas.

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Question = ({ filteredQuestion, setIndexQuestion, indexQuestion, questionsFiltered,
+const Question = ({ filteredQuestion, setIndexQuestion, indexQuestion, questionsFiltered, setActiveQuiz,
 }) => {
+
+  /* Estado para establecer las preguntas random. */
+  const [answersRandom, setAnswersRandom] = useState([])
+
+  useEffect(() => {
+    /* Creamos una constante que guarde las respuestas incorrectas y la correcta. */
+    const answers = [
+      ...filteredQuestion.incorrect_answers,
+      filteredQuestion.correct_answer
+    ]
+
+    setAnswersRandom(answers.sort(() => Math.random() - 0.5)); /* Seteamos esas answers guardadas en la constante. El math.random permite poner las respuestas de forma aleatoria para evitar que siempre quede en el mismo lugar.*/
+  }, [filteredQuestion])
+
+  /* Cambio y avance en la pregunta. */
+  const onNextQuestion = () => {
+    setIndexQuestion(indexQuestion + 1 )
+  }
 
   return (
     <>
@@ -16,28 +34,35 @@ const Question = ({ filteredQuestion, setIndexQuestion, indexQuestion, questions
           <span className="font-bold">{filteredQuestion.difficulty}</span>
         </div>
 
-        <button className="border px-5 py-2 rounded-lg font-bold transition-all hover:bg-yellow-500 hover:text-gray-900">
+        <button className="border px-5 py-2 rounded-lg font-bold transition-all hover:bg-yellow-500 hover:text-gray-900" onClick={() => setActiveQuiz(false)}>
           Reiniciar
         </button>
 
         <div>
           <h1 className="font-bold">
-            Titulo de la pregunta.
+            {filteredQuestion.question}
           </h1>
         </div>
 
         {/* Las respuestas del usuario van aqui */}
         <div className="grid grid-cols-2 gap-5">
           {/* Mapeamos un arreglo de respuesta correcta e incorrectas */}
-          <button className="border p-5 rounded-lg flex justify-center items-center hoover:scale-105">
-            <p className="font-medium text-center text-sm">
-              Alguna Respuesta
-            </p>
-          </button>
+          
+          {answersRandom.map((answerText, index) => (
+            <button 
+              className="border p-5 rounded-lg flex justify-center items-center hoover:scale-105"
+              key={answerText}
+              >
+              <p className="font-medium text-center text-sm">
+                {answerText}
+              </p>
+            </button>
+          ))
+          }
         </div>
 
         {/* Aqui va el condicional para mostrar el boton de siguiente pregunta o el de finalizar. */}
-        <button className="border-2 border-yellow-600 text-yellow-600 rounded-md px-5 py-2 hover:bg-yellow-600 hover:text-black font-medium">
+        <button className="border-2 border-yellow-600 text-yellow-600 rounded-md px-5 py-2 hover:bg-yellow-600 hover:text-black font-medium" onClick={onNextQuestion}>
           Finaliar o siguiente pregunta.
         </button>
       </div>
--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState , useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Question from '../components/Question.jsx';
-import { questions } from '../data.js'; /* Importo el array de preguntas de data.js */
+import { questions , imgs } from '../data.js'; /* Importo el array de preguntas de data.js */
 
 //Función para barajar las preguntas de cada categoria y también reducirla al numero deseado.
 const shuffleArray = (array) => {
@@ -17,12 +17,16 @@ export const CategoryPage = () => {
     //console.log(category)
     //console.log(questions)
 
+    const imageQuiz = imgs.filter(img => img === `/src/assets/${category.toLowerCase()}.png`)
+
     // useState. Tomo de data.js, buscando todos los questions que coincidan con la categoría dada. 
     const [questionsFiltered, setQuestionsFiltered] = useState(
         questions.filter(question => question.category === category)
         )
 
     const [indexQuestion, setIndexQuestion] = useState(0);
+
+    const [ activeQuiz, setActiveQuiz ] = useState(false)
 
     useEffect(() => {
         const newQuestions = shuffleArray(questionsFiltered);
@@ -33,12 +37,32 @@ export const CategoryPage = () => {
 
     return (
         <div className='container flex flex-col items-center justify-center gap-10' style={{height: 'calc(100vh - 5rem)'}}>
-            <Question 
+
+            {activeQuiz ? (<Question 
                 filteredQuestion={questionsFiltered[indexQuestion]} 
                 setIndexQuestion={setIndexQuestion}
                 indexQuestion={indexQuestion}
                 questionsFiltered={questionsFiltered}
-            />
+                setActiveQuiz={setActiveQuiz}
+            />) : (
+                <>
+                    <div className="flex flex-col gap-5">
+                        <h1 className="text-3xl text-teal-900 text-center font-bold">
+                            {category}
+                        </h1>
+                        
+                        <div className="flex justify-center items-center">
+                            <img src={imageQuiz} alt={category} className='w-72' />
+                        </div>
+                    </div>
+
+                    <button className="text-white bg-gray-900 py-2 rounded-lg font-bold px-5 transition-all hover:bg-yellow-500 hover:text-gray-900"
+                        onClick={() => setActiveQuiz(true)}>
+                        Iniciar Quiz
+                    </button>
+                </>
+            ) }
+            
         </div>
     )
 }
